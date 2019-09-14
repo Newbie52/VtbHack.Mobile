dfbfbf38707d321e33d0be5eae1404a89c78b599
egor <egorbirukov1234@gmail.com>
2019-09-14 22:43:06 +0300

    Added appropriate linear gradient

--- screens/BillsScreen.js
+++ screens/BillsScreen.js
@@ -1,12 +1,23 @@
 import {Text, View} from "react-native";
 import React from "react";
+import {LinearGradient} from "expo-linear-gradient";
 
 export class BillsScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Тута будут чеки</Text>
-      </View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'stretch' }}>
+          <LinearGradient colors={['#000000', '#154689']} start={[0,0]} end={[1,1]} style={styles.gradientContainer}>
+            <Text>Тута будут чеки</Text>
+          </LinearGradient>
+        </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  gradientContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
