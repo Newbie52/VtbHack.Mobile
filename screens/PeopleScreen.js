704ee73c5d8c219a8fbaf2be0bba004dd0fcbec4
Максим <azeriker@gmail.com>
2019-09-15 01:51:37 +0300

    changed people screen

--- screens/PeopleScreen.js
+++ screens/PeopleScreen.js
@@ -1,12 +1,46 @@
-import {Text, View} from "react-native";
+import { Text, View } from "react-native";
+import { ListItem } from 'react-native-elements'
 import React from "react";
 
+
+
+
 export class PeopleScreen extends React.Component {
+
+  list = [
+    {
+      name: 'Amy Farha',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      subtitle: 'Vice President'
+    },
+    {
+      name: 'Chris Jackson',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Vice Chairman'
+    },
+  ]
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        
+        <ParticipantCard />
       </View>
     );
   }
 }
+
+export class ParticipantCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false
+    };
+  }
+  render() {
+    return (
+      <View style={{ width: "80%", backgroundColor: "#CAECFF", height: "10%", borderRadius: 20, padding: 10 }} on>
+        <Text>{this.props.name}</Text>
+      </View>
+    );
+  }
+}
+
