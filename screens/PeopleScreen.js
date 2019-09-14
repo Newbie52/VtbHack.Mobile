3e6430c212a8ada12672da4149173f8b36286cab
Unknown <prokoshev-andrejj@rambler.ru>
2019-09-15 02:27:30 +0300

    Add header

--- screens/PeopleScreen.js
+++ screens/PeopleScreen.js
@@ -1,6 +1,6 @@
-import { Text, View, TouchableNativeFeedback } from "react-native";
+import { Text, View, TouchableNativeFeedback, StyleSheet } from "react-native";
 import React from "react";
-
+import {LinearGradient} from "expo-linear-gradient";
 
 
 
@@ -20,7 +20,11 @@ export class PeopleScreen extends React.Component {
   ]
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'stretch' }}>
+        <LinearGradient colors={['#000000', '#154689']} start={[0,0]} end={[1,1]} style={styles.gradientContainer}>
+          <Text style={styles.header}>Участники</Text>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'stretch', color: 'white' }}></View>
+        </LinearGradient>
         <ParticipantCard name={'Джсон стетхем'} />
       </View>
     );
@@ -44,4 +48,29 @@ export class ParticipantCard extends React.Component {
     );
   }
 }
+
+export const pos = {
+  position: 'absolute',
+  top: 0, 
+  alignItems: 'center',
+  justifyContent:'center'
+}
+
+const styles = StyleSheet.create({
+  gradientContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  header: {
+    color: '#FFFFFF',
+    backgroundColor: 'transparent',
+    position: pos.position,
+    top: 36,
+    left: 26,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+});
 
