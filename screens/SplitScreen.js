7a9dd7467d47ed5ded5de2be9d81b36897066a43
egor <egorbirukov1234@gmail.com>
2019-09-15 12:41:18 +0300

    AAAAAAA

--- screens/SplitScreen.js
+++ screens/SplitScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ListView, Text, View, CheckBox } from 'react-native';
+import {ListView, Text, View, CheckBox, Image, TouchableOpacity} from 'react-native';
 
 
 export class SplitScreen extends React.Component {
@@ -12,47 +12,58 @@ export class SplitScreen extends React.Component {
 
     const usersSelected = this.props.navigation.getParam('users', null);
     const billItems = this.props.navigation.getParam('billItems', null);
-    
+
+    const products = billItems.map( (item, index) => {return {id: index, name: item.name, price: item.price, users: []}});
+    const users = usersSelected.map(user => { return {name: user.name, products: []}});
+
     const partDs = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     const itemsDs = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+
     this.state = {
+      selectedUsers: usersSelected,
+      products: products,
+      users: users,
       peopleDataSource: partDs.cloneWithRows(usersSelected),
       itemDataSource: itemsDs.cloneWithRows(billItems),
-      selectedUser: 0
+      selectedUser: 0,
+      checkboxArray: this.createCheckBoxArray(usersSelected, billItems),
+      someValue: false
     }
   };
+
+  createCheckBoxArray(users, bills) {
+    const arr = [];
+    for (let i = 0; i < users.length; i++) {
+      arr.push(Array(bills.length).fill(false));
+    }
+    return arr;
+  }
+
+  addProduct = (index) => {
+    const users = this.state.users;
+    const products = this.state.products;
+
+    users[this.state.selectedUser].products.push(products.filter(item => item.id === index));
+    this.setState({
+      users: users,
+      products: products.filter(item => item.id !== index)
+    })
+  };
+
   render() {
     return (
       <View style={{ paddingTop: 100, flex: 1, flexDirection: 'row' }}>
-        <View style={{ width: "35%" }}>
-          <ListView
-            dataSource={this.state.peopleDataSource}
-            renderRow={
-              (rowData, sectionID, rowID, higlightRow) =>
-                <View style={{ flex: 1, flexDirection: 'row' }}>
-                  <CheckBox
-                    title={rowData.name}
-                    checkedIcon='dot-circle-o'
-                    uncheckedIcon='circle-o'
-                    value={this.state.selectedUser === rowID}
-                    onPress={() => this.setState({ selectedUser: rowID })}
-                  ></CheckBox>
-                  <Text>{rowData.name}</Text>
-                </View>}
-          />
+        <View style={{ flex: 2 }}>
+          { this.state.selectedUsers.map( user => <Text>{user.name}</Text>) }
         </View>
-        <View style={{ width: "65%" }}>
-          <ListView
-            dataSource={this.state.itemDataSource}
-            renderRow={
-              (rowData) => <View style={{ flex: 1, flexDirection: 'row' }}>
-                <Text>{rowData.name}</Text>
-                <Text>{rowData.price}</Text>
-                <CheckBox
-                  value={this.state.checked}
-                  onValueChange={() => this.setState({ checked: !this.state.checked })}></CheckBox>
-              </View>}
-          />
+        <View style={{ flex: 4 }}>
+          { this.state.products.map((product) =>
+            <TouchableOpacity style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}
+                              onPress={() => this.addProduct(product.id)}>
+              <Text>{product.name}</Text>
+              <Text>{product.price}</Text>
+            </TouchableOpacity>
+          )}
         </View>
       </View>
     )
