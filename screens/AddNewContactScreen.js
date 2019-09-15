786abc70ec1e2e2e1aa1d3b8d3ff28c77f59a580
Максим <azeriker@gmail.com>
2019-09-15 09:13:54 +0300

    Added qrcontact screen

---
+++ screens/AddNewContactScreen.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Button } from 'react-native';
+export default class AddNewContactScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            address: this.props.address
+        };
+    }
+    onChanged(text) {
+        this.setState({ name: text });
+    }
+
+    saveContact() {
+        //save contact to local memory
+        //navigate to contacts screen
+    }
+
+    render() {
+        return (
+            <View>
+                <TextInput
+                    onChangeText={(text) => this.onChanged(text)}
+                    value={this.state.name}
+                />
+                <Text>{this.state.address}</Text>
+                <Button onPress={() => this.saveContact()}></Button>
+            </View>
+        )
+    }
+}
