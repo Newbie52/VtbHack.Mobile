ba2831203e28cdfbb55d1f85a4ab3c14b9ad59e6
Unknown <prokoshev-andrejj@rambler.ru>
2019-09-15 10:52:06 +0300

    Fix Участники

--- screens/PeopleScreen.js
+++ screens/PeopleScreen.js
@@ -32,7 +32,7 @@ export class PeopleScreen extends React.Component {
                                 color: '#81818',
                                 fontSize: 18,
                                 fontWeight: 'normal',
-                                paddingTop:5
+                                paddingTop:7
                             }}>Отмена</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.buttonOk} onPress={this.goToNextScreen}>
@@ -41,7 +41,7 @@ export class PeopleScreen extends React.Component {
                             color: '#16ACB8',
                             fontSize: 18,
                             fontWeight: 'normal',
-                            paddingTop:5
+                            paddingTop:7
                            }}>Готово</Text>
             </TouchableOpacity>
           </View>
@@ -60,10 +60,10 @@ export const pos = {
 
 const styles = StyleSheet.create({
   contactsItem : {
-    flex: 0.8,
+    flex: 0.9,
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
-    height: "80%",
+    height: "90%",
     marginTop: 85,
     justifyContent: 'center',
     alignItems: 'stretch',
